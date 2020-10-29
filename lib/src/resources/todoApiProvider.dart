b4d9a30cbad392603703e35cb0f8cf376625ba85
Nanang Rustianto <nanang.programmer@gmail.com>
2020-10-30 00:05:59 +0800

    BLOC HTTP X_API_KEY HEADER

---
+++ lib/src/resources/todoApiProvider.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' show Client;
+import 'package:aztira/src/models/todoModel0.dart';
+
+class TodoApiProvider {
+  Client client = Client();
+  List<Todo> todo;
+
+  //final _url = "https://uncok.jogjaide.web.id/cadmin/native/getAll.php";
+  final _url = "https://uncok.jogjaide.web.id/cadmin/api/todo/all";
+  Future<List<Todo>> fetchTodoList() async {
+    print("masuk fungsi");
+    final response = await client.get(_url, headers: {
+      'x-api-key': '4DE868BD51F2BE7C724A9E9A3415C4BA',
+    });
+    if (response.statusCode == 200) {
+      var jsondata = json.decode(response.body);
+
+      print(jsondata);
+
+      return (jsondata['data']['todo'] as List)
+          .map((data) => new Todo.fromJson(data))
+          .toList();
+
+      //print(todo[0].id);
+      //return compute(todoFromJson, response.body);
+
+    } else {
+      throw Exception("Gagal Data");
+    }
+  }
+}
