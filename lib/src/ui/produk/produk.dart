53fa7c14ff8748fdaa6b897543f5e58c48a16413
nanangtokenku <nanang.tokenku@gmail.com>
2023-04-21 02:46:10 +0800

    Perapian

--- lib/src/ui/produk/produk.dart
+++ lib/src/ui/produk/produk.dart
@@ -1,11 +1,5 @@
-import 'dart:convert';
-
-// import 'package:aztira/src/ui/produk/produk1.dart';
-// import 'package:aztira/src/ui/produk/produk2.dart';
 import 'package:aztira/src/ui/produk/produk1.dart';
 import 'package:flutter/material.dart';
-
-import 'package:http/http.dart' as http;
 import '../../utils/app_tool.dart';
 import 'produk2.dart';
 
@@ -17,22 +11,12 @@ class Produk extends StatefulWidget {
 class _Produk extends State<Produk> with SingleTickerProviderStateMixin {
   //create controller untuk tabBar
   TabController controller;
-  List<dynamic> _data = [];
-
-  Future<void> fetchData() async {
-    final response =
-        await http.get(Uri.parse('https://api.jogjaide.web.id/web/card'));
-    final List<dynamic> data = json.decode(response.body);
-    setState(() {
-      _data = data;
-    });
-  }
 
   @override
   void initState() {
     handleLoginSession(context);
     controller = new TabController(vsync: this, length: 2);
-    fetchData();
+
     //tambahkan SingleTickerProviderStateMikin pada class _HomeState
     super.initState();
   }
@@ -66,57 +50,6 @@ class _Produk extends State<Produk> with SingleTickerProviderStateMixin {
             ],
           ),
         ),
-        //   body: _data.isEmpty
-        //       ? Center(child: CircularProgressIndicator())
-        //       : GridView.builder(
-        //           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        //             crossAxisCount: 3,
-        //             childAspectRatio: 3 / 4,
-        //             mainAxisSpacing: 8,
-        //             crossAxisSpacing: 8,
-        //           ),
-        //           itemCount: _data.length,
-        //           itemBuilder: (context, index) {
-        //             final item = _data[index];
-        //             return Card(
-        //               child: Column(
-        //                 crossAxisAlignment: CrossAxisAlignment.stretch,
-        //                 children: [
-        //                   AspectRatio(
-        //                     aspectRatio: 1,
-        //                     child: Image.asset(
-        //                       //item['url'],
-        //                       ('assets/profile_pic.jpg'),
-        //                       fit: BoxFit.fitHeight,
-        //                     ),
-        //                   ),
-        //                   Padding(
-        //                     padding: const EdgeInsets.all(8.0),
-        //                     child: Text(
-        //                       item['name'],
-        //                       //"Nanang Aja",
-        //                       textAlign: TextAlign.center,
-        //                       style: TextStyle(
-        //                         fontWeight: FontWeight.bold,
-        //                       ),
-        //                     ),
-        //                   ),
-        //                   // Padding(
-        //                   //   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        //                   //   child: Text(
-        //                   //     item['albumId'].toString(),
-        //                   //     style: TextStyle(
-        //                   //       color: Colors.grey[600],
-        //                   //     ),
-        //                   //   ),
-        //                   // ),
-        //                 ],
-        //               ),
-        //             );
-        //           },
-        //         ),
-        // );
-
         body: new TabBarView(
           //controller untuk tab bar
           controller: controller,
